465b57f774156c5269cfd9053637b3246b9ec716
PrathamGhoshRoy <prathamghoshroy@gmail.com>
2024-02-10 21:14:11 +0530

    first commit

--- scripts/sentiment.py
+++ scripts/sentiment.py
@@ -5,3 +5,5 @@ sentiment_classifier = pipeline("sentiment-analysis", verbose=False)
 res = sentiment_classifier("I've been having a really good day, but some bad things happened, but that's okay.")
 
 print(res)
+
+print(type(res))
